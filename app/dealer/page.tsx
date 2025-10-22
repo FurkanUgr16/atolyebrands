82f709c5c2e743500cf77b2a4f6ef9adb57fa4cb
Furkan Uğur <ugurfurkan1645@gmail.com>
2025-10-22 17:43:08 +0300

    something forgotten

--- app/dealer/page.tsx
+++ app/dealer/page.tsx
@@ -1,8 +1,12 @@
+import SignOutButton from '@/components/SignOutButton'
 import React from 'react'
 
 const Dealer = () => {
   return (
-    <div>Dealer</div>
+     <>
+      <h1>Dealer Page</h1>
+      <SignOutButton />
+    </>
   )
 }
 
